969fdf552ce043a4e96e08e57d7b13cbccd36303
kvcsmate <86199067+kvcsmate@users.noreply.github.com>
2025-06-29 10:55:08 +0200

    Add spell HUD

---
+++ Scenes/Nodes/HUD/SpellHUD.cs
@@ -0,0 +1,70 @@
+using Godot;
+using System.Collections.Generic;
+using DungeonsAlltheWayDown.AbilitySystem;
+
+public partial class SpellHUD : Node2D
+{
+    private class Slot
+    {
+        public Sprite2D Icon;
+        public Label KeyLabel;
+    }
+
+    private List<Slot> _slots = new();
+
+    [Export] public Texture2D DefaultIcon;
+
+    public SpellBook SpellBook { get; set; }
+
+    public override void _Ready()
+    {
+        if (DefaultIcon == null)
+        {
+            DefaultIcon = GD.Load<Texture2D>("res://Game Assets/HUD/07.png");
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            var slotNode = GetNode<Node2D>($"Slot{i}");
+            var icon = slotNode.GetNode<Sprite2D>("Icon");
+            var label = slotNode.GetNode<Label>("KeyLabel");
+            _slots.Add(new Slot { Icon = icon, KeyLabel = label });
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        if (SpellBook == null) return;
+
+        for (int i = 0; i < _slots.Count; i++)
+        {
+            var slot = _slots[i];
+            if (SpellBook.Pages.ContainsKey(i))
+            {
+                var spell = SpellBook.Pages[i].Spell;
+                if (slot.Icon.Texture == null)
+                    slot.Icon.Texture = DefaultIcon;
+                slot.KeyLabel.Text = GetKeyName(i);
+                slot.Icon.Modulate = spell.IsReady ? Colors.White : new Color(1,1,1,0.5f);
+            }
+            else
+            {
+                slot.Icon.Texture = DefaultIcon;
+                slot.KeyLabel.Text = GetKeyName(i);
+                slot.Icon.Modulate = new Color(1,1,1,0.25f);
+            }
+        }
+    }
+
+    private string GetKeyName(int index)
+    {
+        return index switch
+        {
+            0 => "Q",
+            1 => "W",
+            2 => "E",
+            3 => "R",
+            _ => string.Empty
+        };
+    }
+}
