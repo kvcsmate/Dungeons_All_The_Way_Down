2a9a0463c24437625244964c57be73c2d6bd5b6d
kvcsmate <kvcsmate98@gmail.com>
2026-07-02 14:09:19 +0200

    Added Status effects

--- BaseClasses/LevelEntities/Character.cs
+++ BaseClasses/LevelEntities/Character.cs
@@ -1,9 +1,7 @@
-﻿using Godot;
+﻿using Dungeons_All_The_Way_Down.BaseClasses.LevelEntities;
+using Godot;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 public abstract partial class Character : LevelEntity
@@ -26,11 +24,77 @@ public abstract partial class Character : LevelEntity
     [Export]
     public string SpriteName;
 
+    private int _activeSlowCount;
+    private float _movementSpeedBeforeSlow;
+
     [Export]
     public float MovementSpeed = 500.0f;
+
+    [Export(PropertyHint.Range, "0,1,0.05")]
+    public float SlowSpeedMultiplier = 0.5f;
 
     [Export]
     public Godot.Collections.Array<PackedScene> Spells;
 
-}
+    private readonly List<Tuple<float, StatusEffect>> _statusEffects = new();
 
+    public IReadOnlyList<Tuple<float, StatusEffect>> StatusEffects => _statusEffects;
+
+    public event Action<StatusEffect, bool> StatusEffectChanged;
+
+    protected Character()
+    {
+        StatusEffectChanged += HandleStatusEffectChanged;
+    }
+
+    // Connect a spell's (timer, statusType) signal to this method.
+    public void ApplyStatusEffect(float timer, StatusEffect statusType)
+    {
+        if (timer <= 0)
+        {
+            return;
+        }
+
+        var activeEffect = new Tuple<float, StatusEffect>(timer, statusType);
+        _statusEffects.Add(activeEffect);
+        StatusEffectChanged?.Invoke(statusType, true);
+        _ = ExpireStatusEffect(activeEffect);
+    }
+
+    private async Task ExpireStatusEffect(Tuple<float, StatusEffect> activeEffect)
+    {
+        SceneTreeTimer timer = GetTree().CreateTimer(activeEffect.Item1);
+        await ToSignal(timer, SceneTreeTimer.SignalName.Timeout);
+
+        if (_statusEffects.Remove(activeEffect))
+        {
+            StatusEffectChanged?.Invoke(activeEffect.Item2, false);
+        }
+    }
+
+    private void HandleStatusEffectChanged(StatusEffect statusType, bool applied)
+    {
+        if (statusType != StatusEffect.Slow)
+        {
+            return;
+        }
+
+        if (applied)
+        {
+            if (_activeSlowCount == 0)
+            {
+                _movementSpeedBeforeSlow = MovementSpeed;
+                MovementSpeed *= Mathf.Clamp(SlowSpeedMultiplier, 0.0f, 1.0f);
+            }
+
+            _activeSlowCount++;
+            return;
+        }
+
+        _activeSlowCount--;
+        if (_activeSlowCount == 0)
+        {
+            MovementSpeed = _movementSpeedBeforeSlow;
+        }
+    }
+}
---
+++ BaseClasses/LevelEntities/StatusEffect.cs
@@ -0,0 +1,13 @@
+namespace Dungeons_All_The_Way_Down.BaseClasses.LevelEntities
+{
+    public enum StatusEffect
+    {
+        Poison,
+        Burn,
+        Freeze,
+        Stun,
+        Bleed,
+        Weak,
+        Slow
+    }
+}
